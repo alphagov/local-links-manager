86d2cae6d20f1e802861a794c2579180a53978dc
Murray Steele <muz@h-lame.com>
2016-09-22 16:24:10 +0000

    Add a dropdown for choosing sort order
    Options for this all come from a hash keyed on the param value.  To
    avoid nils we set the value of the 'default' key to be what is returned
    by the hash by default when an unknown key is requested.  Bit overkill
    for a prototype really.  We can use the hash to build the dropdown
    in the view though, so it's somewhat useful.
    
    We'll add the sorting by broken link counts into the next commit as
    this will require more complex queries.

--- app/controllers/v2/home_controller.rb
+++ app/controllers/v2/home_controller.rb
@@ -2,7 +2,7 @@ module V2
   class HomeController < ApplicationController
     def broken_links
       @local_authorities = LocalAuthority.
-        order(name: :desc)
+        order(current_sort_order[:order_args])
 
       links_by_authority = Link.
           includes(:service, :interaction).
@@ -20,12 +20,59 @@ module V2
     end
 
     def local_authorities
-      @local_authorities = LocalAuthority.order(name: :desc)
+      @local_authorities = LocalAuthority.order(current_sort_order[:order_args])
     end
 
     def services
       @services =
-        Service.order(lgsl_code: :desc)
+        Service.order(current_sort_order[:order_args])
     end
+
+    def self.default_sort_order
+      @_default_sort_order ||= {
+        'services' => {
+          'default' => {
+            description: 'Default (code)',
+            param: 'default',
+            order_args: Service.arel_table['lgsl_code'].asc
+          },
+          'alphabetical' => {
+            description: 'A-Z',
+            param: 'alphabetical',
+            order_args: Service.arel_table['label'].asc
+          },
+        }.tap { |services_hash|
+          services_hash.default = services_hash['default']
+        },
+        'local_authorities' => {
+          'default' => {
+            description: 'Default (A-Z)',
+            param: 'default',
+            order_args: LocalAuthority.arel_table[:name].asc
+          },
+        }.tap { |local_authorities_hash|
+          local_authorities_hash.default = local_authorities_hash['default']
+        },
+        'broken_links' => {
+          'default' => {
+            description: 'Default (A-Z)',
+            param: 'default',
+            order_args: LocalAuthority.arel_table[:name].asc
+          },
+        }.tap { |broken_links_hash|
+          broken_links_hash.default = broken_links_hash['default']
+        }
+      }
+    end
+
+    def current_sort_order
+      self.class.default_sort_order[action_name][params.fetch(:sort_order, 'default')]
+    end
+    helper_method :current_sort_order
+
+    def all_sort_order_options
+      self.class.default_sort_order[action_name].values
+    end
+    helper_method :all_sort_order_options
   end
 end
