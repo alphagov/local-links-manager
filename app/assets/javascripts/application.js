671de74c88ce4f90ab91fdacfe3745754440f362
Murray Steele <muz@h-lame.com>
2016-09-27 16:06:55 +0000

    Update total counts and row counts as we filter on the page
    When the filters on the page update, make sure that the on screen
    numbers also update.  For those indexes (broken links + services)
    that display intermediate counts, also update those rows too.
    
    We also change the filtering JS to not treat the la header rows
    on the broken links + services as special cases, only data rows will
    be hidden or shown, and we leave it up to the pages to handle their
    parent rows manually.  This means they no longer need to treat the
    'local-authority-id' selectedType as special case.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,11 @@
-function setupFilterDropdown(rowSelector, placeholderText, doneCallback) {
+function setupFilterDropdown(rowSelector, placeholderText, filterCallback, unfilterCallback) {
+  function setTotalRows(countFunc) {
+    var $totalRows = $('[data-total-rows]')
+    if ($totalRows.length > 0) {
+      var count = countFunc($totalRows);
+      $totalRows.find('.big-number').html(count);
+    }
+  }
   $( "#filter-dropdown" ).select2({
     placeholder: placeholderText,
     allowClear: true,
@@ -22,21 +29,24 @@ function setupFilterDropdown(rowSelector, placeholderText, doneCallback) {
       var selectedItem = e.params.data.element.value.toString().split(':'),
         selectedType = selectedItem[0],
         selectedId = selectedItem[1];
-      $(rowSelector+"[data-"+selectedType+"][data-"+selectedType+"='"+selectedId+"']").show();
-      $(rowSelector+"[data-"+selectedType+"]:not([data-"+selectedType+"='"+selectedId+"'])").hide();
-      doneCallback(selectedType, selectedId);
+      $(rowSelector+"[data-row][data-"+selectedType+"][data-"+selectedType+"='"+selectedId+"']").show();
+      $(rowSelector+"[data-row][data-"+selectedType+"]:not([data-"+selectedType+"='"+selectedId+"'])").hide();
+      filterCallback(selectedType, selectedId);
     } else {
       var url = e.params.data.text;
-      $(rowSelector + "[data-url][data-url^='"+url+"']:hidden").show();
-      $(rowSelector + "[data-url]:not([data-url^='"+url+"']):visible").hide();
-      doneCallback('url', url);
+      $(rowSelector + "[data-row][data-url][data-url^='"+url+"']:hidden").show();
+      $(rowSelector + "[data-row][data-url]:not([data-url^='"+url+"']):visible").hide();
+      filterCallback('url', url);
     }
+    setTotalRows(function(_$totalRows) { return $(rowSelector+'[data-row]:visible').length; });
   }).on("select2:unselect", function (e) {
     $(rowSelector+":hidden").show();
+    setTotalRows(function($totalRows) { return $totalRows.data('totalRows'); });
+    unfilterCallback();
   });
 };
 function setupLinkTypeFilterRadioButtons() {
   $( "input[name=link_type]" ).on("change", function (e) {
-    window.location.href = $(e.target).parent('a').attr('href');
+    window.location.href = $(e.target).closest('a').attr('href');
   })
 };
