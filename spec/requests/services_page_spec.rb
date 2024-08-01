1140b9e57e07c2605d98473da827d0489650459f
ramya vidapanakal <ramya.vidapanakal@digital.cabinet-office.gov.uk>
2024-08-01 11:11:18 +0100

    Added dept user test for service page

---
+++ spec/requests/services_page_spec.rb
@@ -0,0 +1,19 @@
+RSpec.describe "ServicesPage" do
+    include AuthenticationControllerHelpers
+    before do
+      create(:service, label: "Aardvark Wardens", organisation_slugs: %w[department-of-aardvarks])
+    end
+
+    context "with a user from a particular department" do
+      before do
+        login_as_user_from(organisation_slug: "department-of-aardvarks")
+      end
+  
+      it "shows the page of the particular department" do
+        get "/services/aardvark-wardens"
+  
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+  
