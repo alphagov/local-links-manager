7054d9c23a8cd1e85969c1a922e3e883944040b0
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-08-05 16:20:55 +0100

    Allow menu to use gds_editor?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   include GDS::SSO::ControllerMethods
   include ServicePermissions
 
+  helper_method :gds_editor?
+
   before_action :authenticate_user!
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
