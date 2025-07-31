545cddfbe28a556078640b1caa552e50b66fca98
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2025-07-31 15:54:29 +0100

    Improve missing_links.rb line coverage
    Remove self method which is only used by the rake task. We can
    directly call #add_missing_links instead.
    
    Increases coverage to 100%

--- spec/tasks/missing_links_spec.rb
+++ spec/tasks/missing_links_spec.rb
@@ -3,11 +3,15 @@ require "rake"
 
 RSpec.describe "import:missing_links" do
   before do
-    allow(LocalLinksManager::Import::MissingLinks).to receive(:add)
+    importer = instance_double(LocalLinksManager::Import::MissingLinks)
+
+    allow(importer).to receive(:add_missing_links)
+    allow(LocalLinksManager::Import::MissingLinks).to receive(:new).and_return(importer)
   end
 
   it "calls LocalLinksManager::Import::MissingLinks.add" do
     Rake::Task["import:missing_links"].invoke
-    expect(LocalLinksManager::Import::MissingLinks).to have_received(:add)
+
+    expect(LocalLinksManager::Import::MissingLinks.new).to have_received(:add_missing_links)
   end
 end
